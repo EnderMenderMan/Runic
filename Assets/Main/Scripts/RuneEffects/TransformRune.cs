608630f46eb2cc9cae95c34e2af61eba169af39d
JessicaTheEm2 <a24chrca@student.his.se>
2025-11-28 14:43:50 +0100

    Update TransformRune.cs

--- Assets/Main/Scripts/RuneEffects/TransformRune.cs
+++ Assets/Main/Scripts/RuneEffects/TransformRune.cs
@@ -1,19 +1,29 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
 public class TransformRune : Rune
 {
-[SerializeField] private FilterType TranformFilterType;
+    [SerializeField] private FilterType transformFilterType;
+    [SerializeField] private string[] transformFilter;
+    [SerializeField] private int[] transformItemIndexOffsets;
 
-     public override void TriggerRunePlacement(int itemIndex, Alter[] alters) 
-{
- for (int i = 0; i < alters.Length; i++)
+    public override void TriggerRunePlacement(int itemIndex, Alter[] alters)
+    {
+        if (transformItemIndexOffsets.Length == 0)
+        {
+            for (int i = 0; i < alters.Length; i++)
             {
-                if (i == itemIndex)
+                if (transformFilterType == FilterType.Exclusive &&
+                    alters[i].equippedRune.tags.Contains(transformFilter) == true)
                     continue;
-                if (alters[i].equippedRune == null)
+                if (transformFilterType == FilterType.Inclusive &&
+                    alters[i].equippedRune.tags.Contains(transformFilter) == false)
                     continue;
-               
+                Debug.Log("Transformrune");
+                
             }
-}
+            
+        }
+    }
 }
