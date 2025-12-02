b25548331b8fc4b6e1a96b1a7047448bce01438a
JessicaTheEm2 <a24chrca@student.his.se>
2025-12-02 16:37:45 +0100

    h

--- Assets/Main/Scripts/Runes/Swap.cs
+++ Assets/Main/Scripts/Runes/Swap.cs
@@ -1,6 +1,9 @@
+using System;
 using UnityEngine;
-
+using UnityEngine;
+using UnityEngine.Events;
 public class Swap : Rune
 {
-   
+    [Tooltip("When player places the rune on alter")] public UnityEvent onAlterPlaced;
+  //  public void SwapSelectedRune() => SwapSelectedRune((selectedRuneIndex + 1) % transformToRunes.Length);
 }
