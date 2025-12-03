9d0ff36517eeeaad7ff732d82da8b457114f4c0d
JessicaTheEm2 <a24chrca@student.his.se>
2025-12-03 15:51:23 +0100

    e

--- Assets/Main/Scripts/Runes/Swap.cs
+++ Assets/Main/Scripts/Runes/Swap.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 using UnityEngine.Events;
 public class Swap : Rune
 {
-    public override void OnAlterPlace()
+    public override void TriggerRunePlacement(int itemIndex, Alter[] alters)
     {
+       // base.TriggerRunePlacement(itemIndex, alters);
         Console.WriteLine("hi");
         Debug.Log("hiii");
         //alter.equippedRune.transform.position 
-      // if (alter.clusterIndex == 0)
-            transform.position = alter.equippedRune.transform.position;
-            alter.equippedRune.transform.position = transform.position;
+        // if (alter.clusterIndex == 0)
+        //transform.position = alter.equippedRune.transform.position;
+        //alter.equippedRune.transform.position = transform.position;
 
-            Console.WriteLine("hi");
-            Debug.Log("hiii");
-        
+        Console.WriteLine("hi");
+        Debug.Log("hiii");
+
     }
 }
