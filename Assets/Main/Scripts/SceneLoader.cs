1fcefd1c595bde1c53f30c52a518232166e588ec
JessicaTheEm2 <a24chrca@student.his.se>
2025-12-08 15:30:09 +0100

    Update SceneLoader.cs

--- Assets/Main/Scripts/SceneLoader.cs
+++ Assets/Main/Scripts/SceneLoader.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Escape))
+      LoadScene(0);
+  }
+
+  public void TestLog(string input = "TEST")
+  {
+    Debug.Log(input);
+  }
+
+  public void LoadScene(int buildIndex)
+  {
+    SceneManager.LoadScene(buildIndex);
+  }
   public void QuitGame()
   {
     Application.Quit();
