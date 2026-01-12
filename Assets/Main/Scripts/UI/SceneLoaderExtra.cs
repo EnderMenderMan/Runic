cf16f1539ea32ac679dae704e56681e7174b3c17
EnderMenderMan <killerendermenderman@gmail.com>
2026-01-12 11:56:41 +0100

    fixes

--- Assets/Main/Scripts/UI/SceneLoaderExtra.cs
+++ Assets/Main/Scripts/UI/SceneLoaderExtra.cs
@@ -5,14 +5,21 @@ public class SceneLoaderExtra : MonoBehaviour
 {
     [Tooltip("When you press the escape button")] public UnityEvent OnESCPressed;
     [Tooltip("When you press the J button")] public UnityEvent OnJPressed;
+    [Tooltip("Trigges of gameobject is enabled when difficulty is set to hard")] public UnityEvent OnHardDifficulty;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             OnESCPressed.Invoke();
-        if (Input.GetKeyDown(KeyCode.J))
+        if (GameData.difficulty != GameData.Difficulty.Hard && Input.GetKeyDown(KeyCode.J))
             OnJPressed.Invoke();
     }
     public void ChangeDifficulty(int value) => GameData.difficulty = (GameData.Difficulty)value;
+
+    void OnEnable()
+    {
+        if (GameData.difficulty == GameData.Difficulty.Hard)
+            OnHardDifficulty.Invoke();
+    }
 
     //public void ContinuesGame(MenuScritablleObject menuScritablleObject) { MenuScritablleObject.continueGame = true; }
 }
