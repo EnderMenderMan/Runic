a3fbdbaaff6161b6ee757235b67f91e044e845d3
JessicaTheEm2 <a24chrca@student.his.se>
2026-01-13 13:04:39 +0100

    outro

--- Assets/Main/Scripts/UI/VideoScript.cs
+++ Assets/Main/Scripts/UI/VideoScript.cs
@@ -1,23 +1,23 @@
 
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class videoscript : MonoBehaviour
 {
 
-    VideoPlayer video;
+    [SerializeField] VideoPlayer video;
+    [SerializeField] int buildIndex;
 
     private void Awake()
     {
-        video = GetComponent<VideoPlayer>();
         video.Play();
         video.loopPointReached += CheckOver;
     }
-
-
+    
     private void CheckOver(UnityEngine.Video.VideoPlayer vp)
     {
-        SceneManager.LoadScene(7);
+        SceneManager.LoadScene(buildIndex);
     }
 }
